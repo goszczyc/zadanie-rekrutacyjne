4458a7ea7aec9d09aeb6cb52c02baeba9260f569
goszczyc <szymon.goszczycki@gmail.com>
2023-04-21 10:32:51 +0200

    removed default values for content

--- template-parts/about.php
+++ template-parts/about.php
@@ -1,30 +1,25 @@
 <?php
-$about_heading = get_field('about_heading') ? get_field('about_heading') : 'Ubezpieczenia majątkowe';
-$about_subheading = get_field('about_subheading') ? get_field('about_subheading') : 'Co obejmują?';
-$about_content = get_field('about_content') ? get_field('about_content') : '<p>
-<strong>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-</strong>
-</p>
-<p>
-Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
-</p>';
+$about_heading = get_field('about_heading');
+$about_subheading = get_field('about_subheading');
+$about_content = get_field('about_content');
 ?>
-
-<section class="about relative max-w-[1920px] mx-auto">
-    <img src="<?= get_template_directory_uri(); ?>/dist/images/about-bg.jpg" alt="" class="hidden xs:block absolute bottom-0 right-0 w-full -z-10">
-    <div class="container grid grid-cols-4 gap-4 px-4 mx-auto">
-        <div class="col-span-full md:col-span-3 pt-[4.5rem] pb-24 text-center xs:text-left">
-            <div class="mb-6">
-                <h2 class="text-primary font-quicksand font-bold text-5xl sm:text-6xl lg:text-[63px]/[68px] mb-2">
-                    <?= $about_heading ?>
-                </h2>
-                <p class="text-primary font-quicksand font-bold text-2xl sm:text-3xl lg:text-[34px]/[40px]"><?= $about_subheading; ?></p>
+<?php if ($about_heading && $about_content) : ?>
+    <section class="about relative max-w-[1920px] mx-auto">
+        <img src="<?= get_template_directory_uri(); ?>/dist/images/about-bg.jpg" alt="" class="hidden xs:block absolute bottom-0 right-0 w-full -z-10">
+        <div class="container grid grid-cols-4 gap-4 px-4 mx-auto">
+            <div class="col-span-full md:col-span-3 pt-[4.5rem] pb-24 text-center xs:text-left">
+                <div class="mb-6">
+                    <h2 class="text-primary font-quicksand font-bold text-5xl sm:text-6xl lg:text-[63px]/[68px] mb-2">
+                        <?= $about_heading ?>
+                    </h2>
+                    <p class="text-primary font-quicksand font-bold text-2xl sm:text-3xl lg:text-[34px]/[40px]"><?= $about_subheading; ?></p>
+                </div>
+                <div class="mb-14 prose max-w-none prose-headings:text-primary prose-headings:font-quicksand prose-p:text-[17px]/[28px] prose-p:text-black prose-p:mb-6 prose-p:mt-0">
+                    <?= $about_content; ?>
+                </div>
+                <a href="#" class="btn btn--orange btn--secondary mx-auto xs:mx-0">Zamów ubezpieczenie</a>
             </div>
-            <div class="mb-14 prose max-w-none prose-headings:text-primary prose-headings:font-quicksand prose-p:text-[17px]/[28px] prose-p:text-black prose-p:mb-6 prose-p:mt-0">
-                <?= $about_content; ?>
-            </div>
-            <a href="#" class="btn btn--orange btn--secondary mx-auto xs:mx-0">Zamów ubezpieczenie</a>
         </div>
-    </div>
-</section>
+    </section>
+
+<?php endif; ?>
--- template-parts/hero.php
+++ template-parts/hero.php
@@ -4,28 +4,30 @@ $hero_heading = get_field('hero_heading') ? get_field('hero_heading') : 'Ubezpie
 $hero_subheading = get_field('hero_subheading') ? get_field('hero_subheading') : 'Skontaktuj się z nami i skorzystaj ze specjalnej oferty!';
 $form_heading = get_field('form_heading') ? get_field('form_heading') : 'Podaj dane kontaktowe, a my przygotujemy najlepszą ofertę';
 ?>
-
-<section class="bg-cover bg-center bg-no-repeat" style="background-image: url(<?= $hero_bg_image; ?>);">
-    <div class="max-w-[100vw] overflow-hidden">
-        <div class="container mx-auto grid grid-cols-1 xs:grid-cols-10  md:grid-cols-2 lg:grid-cols-12 gap-4 px-5 items-end">
-            <div class="relative w-full col-span-full md:col-span-1 lg:col-span-7 before:absolute before:bottom-0 before:left-1/2 before:w-[300vw] before:-translate-x-1/2 before:bg-primary before:opacity-75 before:h-full">
-                <div class="relative py-16 text-center md:text-left">
-                    <h1 class="text-4xl sm:text-[52px]/[56px] text-secondary font-bold font-quicksand mb-2">
-                        <?= $hero_heading; ?>
-                    </h1>
-                    <p class="text-xl sm:text-3xl text-white font-bold font-quicksand max-w-lg mx-auto md:mx-0">
-                        <?= $hero_subheading; ?>
-                    </p>
+<?php if ($hero_heading) : ?>
+    <section class="bg-cover bg-center bg-no-repeat" <?php if ($hero_bg_image) : ?> style="background-image: url(<?= $hero_bg_image; ?>);" <?php endif; ?>>
+        <div class="max-w-[100vw] overflow-hidden">
+            <div class="container mx-auto grid grid-cols-1 xs:grid-cols-10  md:grid-cols-2 lg:grid-cols-12 gap-4 px-5 items-end">
+                <div class="relative w-full col-span-full md:col-span-1 lg:col-span-7 before:absolute before:bottom-0 before:left-1/2 before:w-[300vw] before:-translate-x-1/2 before:bg-primary before:opacity-75 before:h-full">
+                    <div class="relative py-16 text-center md:text-left">
+                        <h1 class="text-4xl sm:text-[52px]/[56px] text-secondary font-bold font-quicksand mb-2">
+                            <?= $hero_heading; ?>
+                        </h1>
+                        <p class="text-xl sm:text-3xl text-white font-bold font-quicksand max-w-lg mx-auto md:mx-0">
+                            <?= $hero_subheading; ?>
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="w-full  xs:col-span-8 xs:col-start-2 sm:col-start-3 sm:col-span-6 md:col-span-1 lg:col-span-5 py-16">
-                <div class="contact-form bg-white rounded-[10px] p-8">
-                    <h2 class="text-xl text-center md:text-left sm:text-2xl text-primary font-bold font-quicksand mb-5">
-                        <?= $form_heading; ?>
-                    </h2>
-                    <?= do_shortcode('[contact-form-7 id="18" title="Contact-form"]'); ?>
+                <div class="w-full  xs:col-span-8 xs:col-start-2 sm:col-start-3 sm:col-span-6 md:col-span-1 lg:col-span-5 py-16">
+                    <div class="contact-form bg-white rounded-[10px] p-8">
+                        <h2 class="text-xl text-center md:text-left sm:text-2xl text-primary font-bold font-quicksand mb-5">
+                            <?= $form_heading; ?>
+                        </h2>
+                        <?= do_shortcode('[contact-form-7 id="18" title="Contact-form"]'); ?>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+
+<?php endif; ?>
